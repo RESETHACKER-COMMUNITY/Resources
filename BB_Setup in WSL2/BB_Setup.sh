489970915eb9a66d10e334655c29a2ffc5683fcd
resethacker <resetme.vk@gmail.com>
2021-03-14 03:49:55 +0530

    Update BB_Setup.sh

--- BB_Setup in WSL2/BB_Setup.sh
+++ BB_Setup in WSL2/BB_Setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-sudo apt-get -y update && sudo apt-get -y upgrade
-
-
-sudo apt-get install -y libffi-dev libpcap-dev libreadline6-dev libyaml-dev libcurl4-openssl-dev libssl-dev jq libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev libldns-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev lua5.2lua5.2-dev libtoolopenjdk-7-jre python-software-properties sqlite3 subversion wget xtightvncviewer zlib1g-dev
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y git
 sudo apt-get install -y ruby-full
-sudo apt-get install -y python-setuptools
+sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
-sudo apt-get install -y python-pip
+sudo apt-get install -y python-pip3
 sudo apt-get install -y python3-venv
 sudo apt-get install -y python-dnspython
-sudo apt-get install -y git
-sudo apt-get install -y rename autoconf build-essential curl 
+sudo apt-get install -y python-setuptools software-properties-common autoconf build-essential
+sudo apt-get install -y libffi-dev libpcap-dev libreadline-dev libyaml-dev libcurl4-openssl-dev libssl-dev libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev libldns-dev libsqlite3-dev libxml2-dev libxslt1-dev libcurl4-openssl-dev libapr1 libaprutil1 libldns2 libserf-1-1 libsvn1 libutf8proc2 libapache2-mod-svn  
+sudo apt-get install screen wget curl jq rename sqlite3 subversion xtightvncviewer sqlite3-doc db5.3-util subversion-tools tightvncserver ssh build-essential 
 
+#Thanks to Nahamsec for the recon_profile
 echo "installing bash_profile aliases from recon_profile"
 git clone https://github.com/nahamsec/recon_profile.git
 cd recon_profile
@@ -33,11 +34,7 @@ select choice in "${choices[@]}"; do
 
 					echo "Installing Go"
 					wget https://golang.org/dl/go1.16.2.linux-amd64.tar.gz
-					sudo tar#install go
-if [[ -z "$GOPATH" ]];then
-echo "It looks like go is not installed, would you like to install it now"
-PS3="Please select an option : "
-choices=("yes -xvf go1.16.2.linux-amd64.tar.gz
+					sudo tar -xvf go1.16.2.linux-amd64.tar.gz
 					sudo mv go /usr/local
 					export GOROOT=/usr/local/go
 					export GOPATH=$HOME/go
@@ -50,7 +47,7 @@ choices=("yes -xvf go1.16.2.linux-amd64.tar.gz
 					break
 					;;
 				no)
-					echo "Please install go and rerun this script"
+					echo "It look like you have already installed the latest version of GO if not then select YES from this script"
 					echo "Aborting installation..."
 					exit 1
 					;;
@@ -61,14 +58,13 @@ fi
 
 #Don't forget to set up AWS credentials!
 echo "Installing AWS Cli"
-apt install -y awscli
 sudo apt-get install -y awscli
-echo "Don't forget to set up AWS credentials!"
+echo "Do not forget to set up AWS credentials!"
 
 
 
 #create a tools folder in ~/
-mkdir -p ~/tools
+mkdir ~/tools
 cd ~/tools/
 
 #install aquatone
@@ -88,12 +84,12 @@ echo "done"
 echo "installing Sublist3r"
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r*
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd ~/tools/
 echo "done"
 
 echo "installing Subfinder"
-go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
 echo "done"
 
 
@@ -106,7 +102,7 @@ echo "done"
 echo "installing wpscan"
 git clone https://github.com/wpscanteam/wpscan.git
 cd wpscan*
-sudo gem install bundler && bundle install --without test
+sudo gem install bundler && bundle config set --local without 'test'
 cd ~/tools/
 echo "done"
 
@@ -152,7 +148,7 @@ echo "done"
 echo "installing asnlookup"
 git clone https://github.com/yassineaboukir/asnlookup.git
 cd ~/tools/asnlookup
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 cd ~/tools/
 echo "done"
 
@@ -161,11 +157,11 @@ sudo go get -u github.com/tomnomnom/httprobe
 echo "done"
 
 echo "installing unfurl"
-sudo go get -u github.com/tomnomnom/unfurl 
+go get -u github.com/tomnomnom/unfurl 
 echo "done"
 
 echo "installing waybackurls"
-sudo go get github.com/tomnomnom/waybackurls
+go get github.com/tomnomnom/waybackurls
 echo "done"
 
 echo "installing assetfinder"
@@ -185,12 +181,10 @@ export GO111MODULE=on
 go get -v -u github.com/OWASP/Amass/v3/...
 echo "done"
 
-
 echo "installing Arjun"
 git clone https://github.com/s0md3v/Arjun.git
 cd ~/tools/
 echo "done"
-
 
 echo "installing ffuf"
 go get github.com/ffuf/ffuf
@@ -215,6 +209,9 @@ echo "done"
 
 
 
-echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 ls -la
-echo "One last time: don't forget to set up AWS credentials in ~/.aws/!"
+echo "One last time: don not forget to set up AWS credentials in ~/.aws/!"
+
+done
+fi
